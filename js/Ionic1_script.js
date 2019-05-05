72a193e00ac42392edf84782b8c78a6065687d87
lenardblair <49842618+lenardblair@users.noreply.github.com>
2019-05-05 00:40:43 -0500

    Add files via upload

---
+++ js/Ionic1_script.js
@@ -0,0 +1,52 @@
+/* Col 2 -- CrisisGo */
+
+document.getElementById("fabImg1").addEventListener("click", colOne);	
+ function colOne() {
+	document.getElementById('cardImageCol2').src='https://i.imgur.com/W7RCDAa.jpg';	
+}
+document.getElementById("fabImg2").addEventListener("click", colTwo);	
+ function colTwo() {
+	document.getElementById('cardImageCol2').src='https://i.imgur.com/49bs5ir.jpg';	
+}
+document.getElementById("fabImg3").addEventListener("click", colThree);	
+ function colThree() {
+	document.getElementById('cardImageCol2').src='https://i.imgur.com/PGZiqD2.jpg';	
+}
+
+/* Col 3 -- Ketos */
+
+document.getElementById("fabImg1b").addEventListener("click", colOneB);	
+ function colOneB() {
+	document.getElementById('cardImageCol3').src='https://i.imgur.com/iseSnLM.jpg';	
+}
+document.getElementById("fabImg2b").addEventListener("click", colTwoB);	
+ function colTwoB() {
+	document.getElementById('cardImageCol3').src='https://i.imgur.com/yBLpuDl.jpg';	
+}
+document.getElementById("fabImg3b").addEventListener("click", colThreeB);	
+ function colThreeB() {
+	document.getElementById('cardImageCol3').src='https://i.imgur.com/FOGmPJT.jpg';	
+}
+
+/* Col 3 -- SafetyHub1 */
+
+document.getElementById("fabImg1c").addEventListener("click", colOneC);	
+ function colOneC() {
+	document.getElementById('cardImageCol4').src='https://i.imgur.com/yNCoIcC.jpg';	
+}
+document.getElementById("fabImg2c").addEventListener("click", colTwoC);	
+ function colTwoC() {
+	document.getElementById('cardImageCol4').src='https://i.imgur.com/pnTpiz8.jpg';	
+}
+document.getElementById("fabImg3c").addEventListener("click", colThreeC);	
+ function colThreeC() {
+	document.getElementById('cardImageCol4').src='https://i.imgur.com/pkvb7vp.jpg';	
+}
+/* Change PDf Script */
+document.getElementById("randomPdfIcon").addEventListener("click", randomPdf);	
+function myFunction() {
+  var x = document.getElementById("myEmbed").src;
+  document.getElementById("demo").innerHTML = x;
+}
+
+
